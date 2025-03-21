7ad888b7205e6035bd0d4ea490264cf5b36b8602
litaar <lalitahill77@gmail.com>
2025-03-21 12:08:45 +1100

    # validate user log in

--- user_management.py
+++ user_management.py
@@ -4,38 +4,53 @@ import random
 import bcrypt
 
 
-def insertUser(username, password, DoB): #hash password and salt and hash
+# Inserts user info into database
+def insertUser(username, password, DoB):  # hash password and salt and hash
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    bytes = password.encode('utf-8') 
-    salt = bcrypt.gensalt() 
-    hash = bcrypt.hashpw(bytes, salt) 
-    
-    cur.execute(  "INSERT INTO users (username,password,dateOfBirth,salt) VALUES (?,?,?,?)",
-    (username, hash, DoB,salt), )
+    bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+
+    cur.execute(
+        "INSERT INTO users (username,password,dateOfBirth,salt) VALUES (?,?,?,?)",
+        (username, hash, DoB, salt),
+    )
     con.commit()
     con.close()
-    
 
 
+# Get salt from user
+def retrieveSalt(username):
+    con = sql.connect("database_files/database.db")
+    cur = con.cursor()
+    cur.execute(f"SELECT salt FROM users WHERE username = (?)", (username,))
+    return cur.fetchone()[0]
+
+
+# Create a function to hash and salt the users input to validate, hash pass using salt and given input pass
+def hashPass(password, salt):
+    pw = password.encode()
+    salt = bcrypt.salt()
+    hash = bcrypt.hashpw(pw, salt)
+    result = bcrypt.checkpw(hash, salt)
+    return hash
+
+
+# Retrieve user info, validate user
 def retrieveUsers(username, password):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    result=cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
-    
-    print(result.fetchone())
-    #fetch one returns the first row 
-    #validate user sign in 
-    if password == '{hash}': 
-        return True
-    #write the code to now check the two hash values with bcrypt using the saved salt
-    #get the password
-    
-    if cur.fetchone() == None:
+    cur.execute(f"SELECT * FROM users WHERE username = (?)", (username,))
+    if cur.fetchone() == None:  # checks for a match --> no match (none)
         con.close()
         return False
-    else:   
-        
+    else:
+        salt = retrieveSalt(username)
+        print(salt)
+        hashpw = hashPass(salt, password)
+        print(hashpw)
+        # valid username continued, make a get salt function
         cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
         # Plain text log of visitor count as requested by Unsecure PWA management
         with open("visitor_log.txt", "r") as file:
@@ -45,10 +60,6 @@ def retrieveUsers(username, password):
             file.write(str(number))
         # Simulate response time of heavy app for testing purposes
         time.sleep(random.randint(80, 90) / 1000)
-        result = cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
-        print(username,password)
-        print(result.fetchone())
-        
         if cur.fetchone() == None:
             con.close()
             return False
@@ -56,7 +67,12 @@ def retrieveUsers(username, password):
             con.close()
             return True
 
+    # if result == password:
+    # return True
+    hashpw = getHashedPass(salt, password)
 
+
+# print(result)
 
 
 def insertFeedback(feedback):
@@ -78,3 +94,10 @@ def listFeedback():
         f.write(f"{row[1]}\n")
         f.write("</p>\n")
     f.close()
+
+
+# call retrieved users function using hash password
+
+retrieveUsers("hello", "password")
+
+# hashSalt("hello")
